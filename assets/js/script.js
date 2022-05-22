0b64262d7005071a8ed1ca860e37b507bcb71c1c
frodrickfronkenstein <ericandrewsmith88@gmail.com>
2022-05-22 00:25:34 -0400

    some refactoring

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,10 +10,7 @@ var auditTimeBlock = function() {
     var timeBlockHour = $(".time-block")
     for (var i = 0; i <= timeBlockHour.length-1; i++) {
         var thisTimeBlock = timeBlockHour;
-        console.log(thisTimeBlock[i]);
-        console.log(currentHour);
         var blockTime = timeBlockHour[i].id.split("-")[1];
-        console.log(blockTime);
         if (currentHour < blockTime){
             $(thisTimeBlock[i]).addClass("future");
         } else if (currentHour > blockTime) {
@@ -23,6 +20,9 @@ var auditTimeBlock = function() {
         }
     }
 };
+
 setInterval(function() {
     auditTimeBlock();
 },1000 * 60);
+
+auditTimeBlock();
