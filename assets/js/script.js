a65fbc18bd9401ec0e55974b82392b688ce1dc2d
frodrickfronkenstein <ericandrewsmith88@gmail.com>
2022-05-22 10:05:29 -0400

    getting values from textfield

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,9 +3,9 @@ $("#currentDay").text(
     moment().format('dddd, MMMM Do')
 );
 
-// color timeblocks according to time of day
+// gets current hour
 var currentHour = moment().format("H");
-
+// color timeblocks according to time of day
 var auditTimeBlock = function() {
     var timeBlockHour = $(".time-block")
     for (var i = 0; i <= timeBlockHour.length-1; i++) {
@@ -21,6 +21,18 @@ var auditTimeBlock = function() {
     }
 };
 
+// save text field to local storage
+$(".saveBtn").on("click", function(){
+    console.log("clicked");
+    //get text from text area
+    var thisParent = $(this).parent();
+    console.log(thisParent);
+    var txt = thisParent.find(".description").val();
+    console.log(txt);
+
+})
+
+// audits time blocks every minute, because I don't know how to call it every hour on the hour.
 setInterval(function() {
     auditTimeBlock();
 },1000 * 60);
