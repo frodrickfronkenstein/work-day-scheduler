bae73f5d357d47831f2a01dde141b1926ceb0f20
frodrickfronkenstein <ericandrewsmith88@gmail.com>
2022-05-21 23:27:30 -0400

    first dynamic styling added

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,18 @@
+// add current Date to jumbotron
 $("#currentDay").text(
     moment().format('dddd, MMMM Do')
 );
+
+// color timeblocks according to time of day
+var currentHour = moment().format("h");
+
+var auditTimeBlock = function() {
+    var timeBlockHour = $(".time-block")
+    for (var i = 0; i <= timeBlockHour.length-1; i++) {
+        console.log(timeBlockHour[i].id.split("-")[1]);
+        if (currentHour < timeBlockHour[i].id.split("-")[1]){
+            (timeBlockHour).addClass("past");
+        } 
+    }
+};
+auditTimeBlock();
